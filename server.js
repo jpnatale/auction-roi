74653767617ef540e3e23cd23c6c6defbef8af8c
Joe Natale <jpnatale@gmail.com>
2016-12-02 11:58:28 -0500

    more zomg

--- server.js
+++ server.js
@@ -11,10 +11,17 @@ var maxRoiItem = {}
 var maxProfitItem = {}
 var maxRoiKey = ""
 var maxProfitKey = ""
+var database = {
+    remoteUrl : 'mongodb://oldjpnatale:ginger0923@ec2-184-73-108-253.compute-1.amazonaws.com:27017/dummDB',
+    localUrl: 'mongodb://localhost:27017/dummDB'
+};
+var savedOut = "Data has not been pulled since the server was started"
 
 
 
-mongoose.connect('mongodb://oldjpnatale:ginger0923@ec2-184-73-108-253.compute-1.amazonaws.com:27017/dummDB')
+mongoose.connect(database.remoteUrl)
+//mongoose.connect(database.localUrl)
+
 
 var item = require('./models/item.js');
 var best = require('./models/best.js')
@@ -28,30 +35,36 @@ app.get('/recipes', function (req,res){
 	recipes(res)
 })
 
-app.get('/prop', function (req,res){
-	prop(res)
-})
-
 app.get('/best', function (req,res){
 
-	console.log("Pulling Best Choices")
+	console.log("Pulling Best Choices: " + Date())
+	res.json(savedOut)
+	// getBestRoi().then(function(){
+	// 	getBestProfit()}).then(function(){
 
-	getBestRoi().then(getBestProfit()).then(function(){
-		var out = {"ROI":String(maxRoiItem.itemName+ " - "+maxRoiItem.roi),"Profit":String(maxProfitItem.itemName+ " - "+Math.round(maxProfitItem.profit))}
-		res.json(out)
-	})	
+	// 	var out = {"ROI":String(maxRoiItem.itemName+ " - "+maxRoiItem.roi),"Profit":String(maxProfitItem.itemName+ " - "+Math.round(maxProfitItem.profit))}
+	// 	res.json(out)
+	// })	
+})
+
+app.get('/update', function (req, res){
+	update(res)
 })
 
 function getBestRoi(){
 	return new Promise(function(resolve,reject){
-		best.findOne({roiOrProfit:'roi'}, 'itemId', function(err,item){
+		best.findOne({roiOrProfit:'roi'}, function(err,bestRoiItemId){
 			if (err) {res.json(err)}
-
-			maxRoiKey = item.itemId
+				
+			if (bestRoiItemId){
+	
+			maxRoiKey = bestRoiItemId.itemId
+			console.log(maxRoiKey)
 			item.findOne({itemId:maxRoiKey}, function(err,foundItem){
 				maxRoiItem = foundItem
 				resolve()
 			})
+		} else {resolve("Error")}	
 	})
 	})
 }
@@ -59,108 +72,70 @@ function getBestRoi(){
 
 function getBestProfit(){
 	return new Promise(function(resolve,reject){
-		best.findOne({roiOrProfit:'profit'}, 'itemId', function(err,item){
+		best.findOne({roiOrProfit:'profit'}, function(err,bestProfitItemId){
 			if (err) {res.json(err)}
-
-			maxProfitKey = item.itemId
+			if (bestProfitItemId){
+			maxProfitKey = bestProfitItemId.itemId
+			console.log(maxProfitKey)
 			item.findOne({itemId:maxProfitKey}, function(err,foundItem){
 				maxProfitItem = foundItem
 				resolve()
 			})	
+		} else {resolve("Error")}
 	})
 	})
 }
 
-app.get('/update', function (req, res){
-
-	pullData.pullData().then(function(out){
-
-		var allData = out.allData
-console.log("Updating Database:")
-		allItems.forEach(function(eachItem){
-
-			item.findOne({itemId:eachItem},function (err, doc){
-				if (err){res.json(err)}
-
-				if (doc){
-					if(doc.mats){
-					item.findOneAndUpdate({itemId:eachItem}, { $set: {
-						'itemId' : eachItem,
-						'itemName' : allData[eachItem].itemName,
-						'costToBuy' : allData[eachItem].costToBuy,
-						'costToMake' : allData[eachItem].costToMake,
-						'profit' : allData[eachItem].profit,
-						'roi' : allData[eachItem].roi,
-						'mats' : JSON.stringify(allData[eachItem].mats)
-								} },	 
-					{new: true},
-					function (err, dbItem){
-						if(err){
-        				console.log("Something wrong when updating data!");
-    					}
-					})					
-				}}
-			})
-		})
-
-		console.log("Updating Best Choices:")
-
-best.findOneAndUpdate({roiOrProfit:'roi'}, {$set: {itemId:out.roiBody}}, function (err,roiItem){
-	best.findOneAndUpdate({roiOrProfit:'profit'}, {$set: {itemId:out.profitBody}}, function (err,profiItem){
-			res.json(out.bestChoice)
-			})
-
-
-		})
-	})
-})//end of update
-
-function prop(res) {
-	var counter = 0
-	for (var i = 0, len = allItems.length; i < len; i++){
-			item.create({
-				itemId : allItems[i],
-				itemName : itemsOG[allItems[i]].itemName,
-				costToBuy : itemsOG[allItems[i]].costToBuy,
-				costToMake : itemsOG[allItems[i]].costToMake,
-				profit : itemsOG[allItems[i]].profit,
-				roi : itemsOG[allItems[i]].roi,
-				mats : JSON.stringify(itemsOG[allItems[i]].mats)
-			}, function(err,item){
+// function prop(res) {
+// 	var counter = 0
+// 	for (var i = 0, len = allItems.length; i < len; i++){
+// 			item.create({
+// 				itemId : allItems[i],
+// 				itemName : itemsOG[allItems[i]].itemName,
+// 				costToBuy : itemsOG[allItems[i]].costToBuy,
+// 				costToMake : itemsOG[allItems[i]].costToMake,
+// 				profit : itemsOG[allItems[i]].profit,
+// 				roi : itemsOG[allItems[i]].roi,
+// 				mats : JSON.stringify(itemsOG[allItems[i]].mats)
+// 			}, function(err,item){
 				
-				if(err){res.send(err)}
-				counter++
-				if (counter == len){
-					getItems(res)
-				}
-			})
-	}
-}
+// 				if(err){res.send(err)}
+// 				counter++
+// 				if (counter == len){
+// 					getItems(res)
+// 				}
+// 			})
+// 	}
+// }
 
 function recipes(res) {
 
 	var counter = 0
-	
-	for (var i = 0, len = allItems.length; i < len; i++){
 
-		itemsToCheck.findOne({itemId:allItems[i]}, function(err,item){
-			
-			if(!item){
-				itemsToCheck.create({itemID:allItems[i]},function(err,item){
+	allItems.forEach(function(eachItem){
+
+		itemsToCheck.findOne({itemId:eachItem}, function(err,checkedItem){
+			console.log(checkedItem)
+			if(!checkedItem){
+				itemsToCheck.create(
+					{'itemId':eachItem}
+					,function(err,newItem){
 					counter++
-					if (counter == len){
+					console.log(newItem)
+
+					if (counter == allItems.length){
 						getItemsToCheck(res)
 					}
 				})
 			} else {
-				console.log("Item for " + allItems[i] + " already exists.")
+				console.log("Item for " + eachItem + " already exists.")
 				counter ++ 
-				if (counter == len){
+				if (counter == allItems.length){
 						getItemsToCheck(res)
 					}
 			}
 		})
-	}
+	})
 
 
 }
@@ -185,6 +160,7 @@ function getItemsToCheck(res) {
             res.send(err);
         } else {
 
+
         res.json(items)
         } // return all todos in JSON format
     });
@@ -197,8 +173,6 @@ function getBests(res) {
         if (err) {
             res.send(err);
         }
-console.log(bests)
-console.log('here')
         res.json(bests); // return all todos in JSON format
     });
 };
@@ -211,3 +185,109 @@ console.log('here')
 
 	})
 //})
+function update(res) {
+		pullData.pullData().then(function(out){
+
+		var allData = out.allData
+		savedOut = out.bestChoice
+console.log("Updating Database: " + Date())
+		allItems.forEach(function(eachItem){
+
+			item.findOne({itemId:eachItem},function (err, doc){
+				if (err){res.json(err)}
+
+				if (doc){
+				
+					if(doc.mats){
+					item.findOneAndUpdate({itemId:eachItem}, { $set: {
+						'itemId' : eachItem,
+						'itemName' : allData[eachItem].itemName,
+						'costToBuy' : allData[eachItem].costToBuy,
+						'costToMake' : allData[eachItem].costToMake,
+						'profit' : allData[eachItem].profit,
+						'roi' : allData[eachItem].roi,
+						'mats' : JSON.stringify(allData[eachItem].mats)
+								} },	 
+					{new: true},
+					function (err, dbItem){
+						if(err){
+        				console.log("Something wrong when updating data!");
+        				console.log(err)
+    						}
+						})					
+					} else {
+						item.findOneAndUpdate({itemId:eachItem}, { $set: {
+						'itemId' : eachItem,
+						'itemName' : allData[eachItem].itemName,
+						'costToBuy' : allData[eachItem].costToBuy
+								} },	 
+					{new: true},
+					function (err, dbItem){
+						if(err){
+        				console.log("Something wrong when updating the non-mats data!");
+        				console.log(err)
+    						}
+						})
+					}
+				} else {
+
+
+					item.create({
+						'itemId' : eachItem,
+						'itemName' : allData[eachItem].itemName,
+						'costToBuy' : allData[eachItem].costToBuy,
+						'costToMake' : allData[eachItem].costToMake,
+						'profit' : allData[eachItem].profit,
+						'roi' : allData[eachItem].roi,
+						'mats' : JSON.stringify(allData[eachItem].mats)
+								}, function (err, created){
+									console.log("created new item")
+								})
+				}
+			})
+		})
+
+		console.log("Updating Best Choices: " + Date())
+
+	best.findOne({roiOrProfit:'roi'},function (err,ifRoiFound){
+		if(ifRoiFound){
+			best.findOneAndUpdate({roiOrProfit:'roi'}, {$set: {'itemId':out.roiBody}}, function (err,roiItem){
+
+				best.findOne({roiOrProfit:'profit'},function (err,ifProfitFound){
+					if(ifProfitFound){
+						best.findOneAndUpdate({roiOrProfit:'profit'}, {$set: {'itemId':out.profitBody}}, function (err,profitItem){
+							console.log(1)
+							res.json(out.bestChoice)})
+					} else {
+						best.create({'roiOrProfit':'profit','itemId':out.profitBody},function (err, createdProfit){
+							console.log(2)
+							res.json(out.bestChoice)})
+					}
+				})
+
+			})
+
+		} else {
+			best.create(out.roiBody, function (err, createdRoi){
+				console.log(out.roiBody)
+					best.findOne({roiOrProfit:'profit'},function (err,ifProfitFound){
+
+					if(ifProfitFound){
+						best.findOneAndUpdate({roiOrProfit:'profit'}, {$set: {'itemId':out.profitBody}}, function (err,profitItem){
+							console.log(3)
+							res.json(out.bestChoice)})
+					} else {
+						best.create(out.profitBody,function (err, createdProfit){
+							console.log(4)
+							res.json(out.bestChoice)})
+					}
+				})
+			})
+		}
+
+
+
+
+	})
+	})
+}
